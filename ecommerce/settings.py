44facf5b19834a9b8e0971ecba861625e57f2165
vygantas <tulaba@gmail.com>
2024-10-24 10:02:40 +0000

    Initial commit

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -1,15 +1,17 @@
+import os
 from oscar.defaults import *
 from pathlib import Path
 from django.utils.translation import gettext_lazy as _
+import dj_database_url
 
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = 'jūsų-slaptas-raktas-čia'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'jūsų-slaptas-raktas-čia')
 
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',')
 
 
 INSTALLED_APPS = [
@@ -59,6 +61,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     # 'django.middleware.locale.LocaleMiddleware',  # Added LocaleMiddleware
     'django.middleware.common.CommonMiddleware',
@@ -68,6 +71,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'oscar.apps.basket.middleware.BasketMiddleware',
     'django.contrib.flatpages.middleware.FlatpageFallbackMiddleware',
+
 ]
 
 ROOT_URLCONF = 'ecommerce.urls'
@@ -96,11 +100,18 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'ecommerce.wsgi.application'
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default='sqlite:///' + str(BASE_DIR / 'db.sqlite3'),
+        conn_max_age=600
+    )
 }
 
 AUTH_PASSWORD_VALIDATORS = [
@@ -224,3 +235,6 @@ OSCAR_SEND_REGISTRATION_EMAIL = False
 OSCAR_SEND_REGISTRATION_EMAIL = False
 OSCAR_SEND_ORDER_CONFIRMATION_EMAIL = False
 OSCAR_SEND_ORDER_EMAIL_SUBJECT = ''
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
